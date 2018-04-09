6243a32a3237fe7b464eca1a17243058f9d2b159
aclavel <test2@gmail.com>
2018-04-09 12:25:50 +0200

    jkbiouhjkl

--- app/src/main/java/com/example/alexis/projeteuropcar/Activity/UpdateAgenceActivity.java
+++ app/src/main/java/com/example/alexis/projeteuropcar/Activity/UpdateAgenceActivity.java
@@ -1,15 +1,22 @@
 package com.example.alexis.projeteuropcar.Activity;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.alexis.projeteuropcar.Fragment.UpdateAgenceFragment;
 import com.example.alexis.projeteuropcar.R;
 
-public class UpdateAgenceActivity extends AppCompatActivity {
+public class UpdateAgenceActivity extends AppCompatActivity implements UpdateAgenceFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_agence);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
